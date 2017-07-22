224bbe1dee0e7b8dc8a216fc4bf65b7926b402af
ArnaudM <arnaud.mathias@signall.com>
2017-07-22 14:24:04 +0200

    tag project name instead of tellme

--- docker/buildAll.sh
+++ docker/buildAll.sh
@@ -4,11 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR} \
 && echo '//////////////////// BUILDING APP Image \\\\\\\\\\\\\\\\\\\\' \
-&& docker build --build-arg project=$project_name -f app/Dockerfile -t tellme/app . \
+&& docker build --build-arg project=$project_name -f app/Dockerfile -t $project_name/app . \
 && echo '//////////////////// BUILDING PHP-fpm Image \\\\\\\\\\\\\\\\\\\\' \
-&& docker build --build-arg project=$project_name -f php-fpm/Dockerfile -t tellme/php-fpm . \
+&& docker build --build-arg project=$project_name -f php-fpm/Dockerfile -t $project_name/php-fpm . \
 && echo '//////////////////// BUILDING nginx Image \\\\\\\\\\\\\\\\\\\\' \
-&& docker build --build-arg project=$project_name -f nginx/Dockerfile -t tellme/nginx . \
+&& docker build --build-arg project=$project_name -f nginx/Dockerfile -t $project_name/nginx . \
 && docker-compose up -d \
 && docker-compose exec php composer install \
 && cd ../projects/$project_name \
